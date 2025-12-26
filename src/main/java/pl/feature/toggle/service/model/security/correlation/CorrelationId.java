6d76fed609c14837b40a203d658e5b1ffd776465
SaQie <Kamcio99@interia.com>
2025-12-26 19:26:55 +0100

    added validation for correlation id

--- src/main/java/pl/feature/toggle/service/model/security/correlation/CorrelationId.java
+++ src/main/java/pl/feature/toggle/service/model/security/correlation/CorrelationId.java
@@ -1,6 +1,16 @@
 package pl.feature.toggle.service.model.security.correlation;
 
+import pl.feature.toggle.service.model.exception.MissingCorrelationIdException;
+
 public record CorrelationId(
         String value
 ) {
+
+    public static CorrelationId of(String value) {
+        if (value == null || value.isBlank()) {
+            throw new MissingCorrelationIdException();
+        }
+        return new CorrelationId(value);
+    }
+
 }
